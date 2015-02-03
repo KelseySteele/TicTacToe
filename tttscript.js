5ed1abeb40997f902373b4c7d12144aacd61dcde
KelseySteele <kasteele8@gmail.com>
2015-02-02 19:15:46 -0500

    alternate starting players.

--- tttscript.js
+++ tttscript.js
@@ -3,18 +3,24 @@ var player = "X";//First player is X
 var game = {
     numXwins: 0,
     numOwins: 0,
+    firstPlayer: "X",
     restartWins: function(){ //Resets the counter for number of winners. 
         numXwins = 0,
-        numOwins = 0;
-        document.getElementById("displayWins").style.display = "none";
+        numOwins = 0,
+        document.getElementById("areaWins").style.display = "none";
         displayWins();
     },
     displayWins: function(){ //displays the number of wins for X and O.
-        document.getElementById("displayWins").style.display = "block";
+        document.getElementById("areaWins").style.display = "block";
         document.getElementById("Xwins").innerHTML = "X has won " + game.numXwins + " games.";
         document.getElementById("Owins").innerHTML = "O has won " + game.numOwins + " games.";  
     },
-    winner: function(){ //determines winner of the game.
+    firstPlayerDisplay: function(){ //displays the number of wins for X and O.
+        document.getElementById("firstPlayerDisplay").style.display = "none";
+        document.getElementById("firstPlayerDisplay").style.display = "block";
+        document.getElementById("firstPlayerDisplay").innerHTML = game.firstPlayer + " goes first.";  
+    },
+    winner: function() { //determines winner of the game.
         if (
           document.getElementById("btn1").value ==="X" &&//horizontal X wins
           document.getElementById("btn2").value ==="X" &&
@@ -46,12 +52,11 @@ var game = {
        {
            game.numXwins = game.numXwins+1;
            alert("X IS THE WINNER!!! :D");
-           game.displayWins();
-           reset();  //code  
+           game.displayWins();  //code  
        }
        
        else if (
-           document.getElementById("btn1").value ==="O" &&//horizontal O wins
+          document.getElementById("btn1").value ==="O" &&//horizontal O wins
           document.getElementById("btn2").value ==="O" &&
           document.getElementById("btn3").value ==="O" ||
           document.getElementById("btn4").value ==="O" &&
@@ -81,13 +86,28 @@ var game = {
        {
            game.numOwins = game.numOwins+1;
            alert("O IS THE WINNER!!! :D");
-           game.displayWins();
-           game.reset();
+           game.displayWins();  
        }  
-   }    
+   },
+   
+   reset: function() { 
+    var currentbtn = 'btn';
+    var i = 1;
+    for (i=1; i<10; i++) {
+         currentbtn = "btn" + i;
+         document.getElementById(currentbtn).value = "";
+         document.getElementById(currentbtn).disabled = false;   
+     }
+     if (game.firstPlayer === "X") {
+        game.firstPlayer = "O";
+     }
+     else {
+        game.firstPlayer = "X";//code
+     };
+     player = game.firstPlayer;
+    game.firstPlayerDisplay();
+    }
 }
-
-
 
 function clickBtn(btn){ // This function puts in an X or O when one of the squares are clicked. 
     if (player === "X") {
@@ -101,9 +121,8 @@ function clickBtn(btn){ // This function puts in an X or O when one of the squar
     };
     game.winner();
 }
-
     
-function reset() {  //This function clears out all the Xs and Os and starts a new game. 
+/*function reset() {  //This function clears out all the Xs and Os and starts a new game. 
     var currentbtn = 'btn';
     var i = 1;
     for (i=1; i<10; i++) {
@@ -111,20 +130,4 @@ function reset() {  //This function clears out all the Xs and Os and starts a ne
         document.getElementById(currentbtn).value = "";
         document.getElementById(currentbtn).disabled = false;   
     }
-    player = "X";
-}
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
+    player = "X";*/
